59f49713286dcbf67cba3b6d6c2989b5ea5a81cb
Lukz-dev <99230932+Lukz-dev@users.noreply.github.com>
2026-06-09 01:18:07 -0300

    Update page.tsx

--- app/(auth)/plans/page.tsx
+++ app/(auth)/plans/page.tsx
@@ -1,4 +1,4 @@
-import { getServerSession } from "next-auth";
+import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import prisma from "@/lib/prisma";
 import { PlansClient } from "@/components/PlansClient";
